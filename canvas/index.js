c9e79f2d0dd24d46c1e0259f1b956fad942cbb6c
Jose Alberto <80777765+JoseAlbertoRodrigues@users.noreply.github.com>
2023-01-04 15:33:00 -0300

    desenhando o jogador e projeteis

--- canvas/index.js
+++ canvas/index.js
@@ -5,6 +5,7 @@ const c = canvas.getContext('2d')
 canvas.width = innerWidth
 canvas.height = innerHeight
 
+// Construtor do Jogador
 class Player {
     constructor(x, y, radius, color) {
         this.x = x
@@ -22,5 +23,60 @@ class Player {
     }
 }
 
-const player = new Player(100, 100, 30, 'blue')
+// Adicionando Construtor dos Projeteis
+class Projectile {
+    constructor(x, y, radius, color, velocity) {
+        this.x = x
+        this.y = y
+        this.radius = radius
+        this.color = color
+        this.velocity = velocity
+    }
+
+    draw() {
+        c.beginPath()
+        c.arc(this.x, this.y, this.radius, Math.PI * 2, false)
+        c.fillStyle = this.color
+        c.fill()
+    }
+}
+
+// colocar o jogador no centro
+const x = canvas.width / 2
+const y = canvas.height / 2
+
+// const player = new Player(100, 100, 30, 'blue')
+const player = new Player(x, y, 30, 'blue')
 player.draw()
+
+
+addEventListener('click', (event) => {
+    // const projectile = new Projectile(event.clientX, event.clientY, 5, 'red', null)
+    const projectile = new Projectile(canvas.width / 2, canvas.height / 2, 5, 'red', null)
+    projectile.draw()
+})
+
+
+// class SnowMan {
+//     constructor(x, y, radius, color) {
+//         this.x = x
+//         this.y = y
+//         this.radius = radius
+//         this.color = color
+//     }
+
+//     draw() {
+//         c.beginPath()
+//         c.arc(this.x, this.y, this.radius, Math.PI * 2, false)
+//         c.fillStyle = this.color
+//         c.fill()
+//     }
+// }
+
+
+// const snowMan = new SnowMan(400, 450, 150, 'red')
+// const snowMan1 = new SnowMan(400, 350, 100, 'black')
+// const snowMan2 = new SnowMan(400, 250, 60, 'pink')
+// snowMan.draw()
+// snowMan1.draw()
+// snowMan2.draw()
