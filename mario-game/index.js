7a1d8dfe2286351b63abd40e5cecef8f147c7bb4
Jose Alberto <80777765+JoseAlbertoRodrigues@users.noreply.github.com>
2023-01-07 10:09:23 -0300

    Criando o jogador
    OK - 2. Player creation

--- mario-game/index.js
+++ mario-game/index.js
@@ -1,3 +1,26 @@
 const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
 
+canvas.width = innerWidth
+canvas.height = innerHeight
+
+class Player {
+    constructor () {
+        this.position = {
+            x: 100,
+            y: 100
+        }
+        this.width = 30
+        this.height = 30
+    }
+
+    draw() {
+        c.beginPath()
+        c.fillStyle ='red'
+        c.fillRect(this.position.x, this.position.y, this.width, this.height)
+        c.fill
+    }
+}
+const player = new Player()
+player.draw()
+
