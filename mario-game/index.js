ea096f807a866fb17008d5d7f91d948e0bd29a0f
Jose Alberto <80777765+JoseAlbertoRodrigues@users.noreply.github.com>
2023-01-07 14:47:45 -0300

    Plataforma, colisão
    Detecção de da plataforma com o jogador
    
    OK - 5. Platforms

--- mario-game/index.js
+++ mario-game/index.js
@@ -31,7 +31,6 @@ class Player {
         this.position.x += this.velocity.x
         this.position.y += this.velocity.y
 
-        
         if(this.position.y + this.height + this.velocity.y <= canvas.height) {
             this.velocity.y += gravity
         } else {
@@ -40,7 +39,29 @@ class Player {
     }
 }
 
+class Platform {
+    constructor() {
+        this.position = {
+            x: 200,
+            y: 100
+        }
+        
+        this.width = 200
+        this.height = 20
+    }
+
+    draw() {
+        // c.beginPath()
+        c.fillStyle = 'green'
+        c.fillRect(this.position.x, this.position.y, this.width, this.height)
+        // c.fill()
+
+    }
+}
+
 const player = new Player()
+const platform = new Platform()
+
 const keys = {
     right: {
         pressed: false
@@ -54,7 +75,9 @@ function animate() {
     requestAnimationFrame(animate)
     c.clearRect(0, 0, canvas.width, canvas.height)
     player.update()
+    platform.draw()
 
+    // key Pressed
     if (keys.right.pressed) {
         player.velocity.x = 5
     } else if (keys.left.pressed) {
@@ -62,6 +85,16 @@ function animate() {
     } else {
         player.velocity.x = 0
     }
+
+    // platform collision detection
+    if (player.position.y + player.height <= platform.position.y &&
+        player.position.y + player.height + player.velocity.y >= platform.position.y &&
+        player.position.x + player.width >= platform.position.x &&
+        player.position.x <= platform.position.x + platform.width) {
+        player.velocity.y = 0
+    }
+
+
 }
 
 animate()
