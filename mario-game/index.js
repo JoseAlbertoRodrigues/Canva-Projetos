83b172df81923337adedc5b0f9cd0651a3695811
Jose Alberto <80777765+JoseAlbertoRodrigues@users.noreply.github.com>
2023-01-07 10:34:53 -0300

    adicionei a gravidade para o jogador
    OK - 3. Gravity

--- mario-game/index.js
+++ mario-game/index.js
@@ -4,11 +4,16 @@ const c = canvas.getContext('2d')
 canvas.width = innerWidth
 canvas.height = innerHeight
 
+const gravity = 0.5
 class Player {
     constructor () {
         this.position = {
             x: 100,
             y: 100
+        }
+        this.velocity = {
+            x: 0,
+            y: 0
         }
         this.width = 30
         this.height = 30
@@ -18,9 +23,28 @@ class Player {
         c.beginPath()
         c.fillStyle ='red'
         c.fillRect(this.position.x, this.position.y, this.width, this.height)
-        c.fill
+        c.fill()
+    }
+
+    update() {
+        this.draw()
+        this.position.y += this.velocity.y
+
+        
+        if(this.position.y + this.height + this.velocity.y <= canvas.height) {
+            this.velocity.y += gravity
+        } else {
+            this.velocity.y = 0
+        }
     }
 }
-const player = new Player()
-player.draw()
 
+const player = new Player()
+
+function animate() {
+    requestAnimationFrame(animate)
+    c.clearRect(0, 0, canvas.width, canvas.height)
+    player.update()
+}
+
+animate()
