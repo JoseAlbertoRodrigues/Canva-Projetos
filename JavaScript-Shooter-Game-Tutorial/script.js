066e7ed525a9c0aa15d6de8d938b35e8efa5b38b
Jose Alberto <80777765+JoseAlbertoRodrigues@users.noreply.github.com>
2023-01-16 16:17:37 -0300

    primeiros passos

--- JavaScript-Shooter-Game-Tutorial/script.js
+++ JavaScript-Shooter-Game-Tutorial/script.js
@@ -4,3 +4,36 @@ const c = canvas.getContext('2d')
 canvas.width = innerWidth
 canvas.height = innerHeight
 
+let timeToNextRaven = 0 // tempo para o próximo raven
+let ravenInterval = 500 // 500 milissegundos
+let lastTime = 0 // variavel para manter o valor do registro data/hora do loop anterior
+
+let ravens = []
+
+class Raven {
+    constructor() {
+        this.width = 100
+        this.height = 50
+        this.x = canvas.width
+        this.y = Math.random() * (canvas.height - this.height)
+        this.directionX = Math.random() * 5 + 3 // movimento, velocidade horizontal
+        this.directionY = Math.random() * 5 - 2.5 // fica subindo e decendo o personagem, eixo y
+    }
+    update() {
+        this.x -= this.directionX
+    }
+
+    draw() {
+        c.fillRect(this.x, this.y, this.width, this.height)
+    }
+}
+
+const raven = new Raven()
+
+function animate(timestamp) {
+    requestAnimationFrame(animate)
+    c.clearRect(0, 0, canvas.width, canvas.height)
+
+}
+
+animate()
