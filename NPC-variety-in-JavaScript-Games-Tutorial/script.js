ab33a45675a836bee50ebd279baecc3d87f8378f
Jose Alberto <80777765+JoseAlbertoRodrigues@users.noreply.github.com>
2023-01-18 22:04:06 -0300

    load event
    adicionando evento para ser iniciado o jogo somente quando tudo estiver carregado.

--- NPC-variety-in-JavaScript-Games-Tutorial/script.js
+++ NPC-variety-in-JavaScript-Games-Tutorial/script.js
@@ -1,51 +1,53 @@
-// const canvas = document.getElementById('canvas1')
-const canvas = document.querySelector('canvas')
-const c = canvas.getContext('2d')
+addEventListener('load', function() {
+    // const canvas = document.getElementById('canvas1')
+    const canvas = document.querySelector('canvas')
+    const c = canvas.getContext('2d')
 
-canvas.width = 500
-canvas.height = 800
+    canvas.width = 500
+    canvas.height = 800
 
-class Game {
-    // aqui terá: atualização de todo o jogo, inimigo, jogadores, obstáculos, menus de fundo, e assim por diante
-    constructor() {
-        this.enemies = []
+    class Game {
+        // aqui terá: atualização de todo o jogo, inimigo, jogadores, obstáculos, menus de fundo, e assim por diante
+        constructor() {
+            this.enemies = []
+        }
+
+        update() {
+
+        }
+
+        draw() {
+
+        }
+
+        #addNewEnemy() { 
+            // Método privado, só pode ser chamado de dentro da minha classe de jogo para gerenciar
+            // algum tipo de funcionalidade interna, em nosso casso adicionar um novo inimigo, na matriz this.enemies
+
+        }
     }
 
-    update() {
+    class Enemy {
+        // aqui será cada inimigo individual
+        constructor() {
 
+        }
+
+        update() {
+
+        }
+
+        draw() {
+
+        }
     }
 
-    draw() {
+    function animate() {
+        requestAnimationFrame(animate)
+        c.clearRect(0, 0, canvas.width, canvas.height)
 
+        c.fillRect(50,50, 100, 100)
     }
 
-    #addNewEnemy() { 
-        // Método privado, só pode ser chamado de dentro da minha classe de jogo para gerenciar
-        // algum tipo de funcionalidade interna, em nosso casso adicionar um novo inimigo, na matriz this.enemies
-
-    }
-}
-
-class Enemy {
-    // aqui será cada inimigo individual
-    constructor() {
-
-    }
-
-    update() {
-
-    }
-
-    draw() {
-
-    }
-}
-
-function animate() {
-    requestAnimationFrame(animate)
-    c.clearRect(0, 0, canvas.width, canvas.height)
-
-    c.fillRect(50,50, 100, 100)
-}
-
-animate()
+    animate()
+})
